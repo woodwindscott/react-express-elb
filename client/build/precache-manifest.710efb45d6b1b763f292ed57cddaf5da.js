2ef252d517d7aabbfea65f9c826db831a9064ad1
William To <towilliam03@gmail.com>
2019-04-07 18:58:26 -0400

    Build server and add .ebextensions

---
+++ client/build/precache-manifest.710efb45d6b1b763f292ed57cddaf5da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "415c5cc38c9c25e6ef37",
+    "url": "/static/js/main.a6b31bb3.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "415c5cc38c9c25e6ef37",
+    "url": "/static/css/main.aa16fa16.chunk.css"
+  },
+  {
+    "revision": "27b99154f979ce5282600e24d4c603ca",
+    "url": "/index.html"
+  }
+];
